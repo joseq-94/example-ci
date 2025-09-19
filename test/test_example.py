212acd91cb284b486ac8b57a8a174b42f8b1c416
Jose Luis Quintero <20149097@tafe.wa.edu.au>
2025-09-20 07:29:39 +0800

    create for activity 2

---
+++ test/test_example.py
@@ -0,0 +1,10 @@
+import add from example
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add('space', 'ship') == 'spaceship'
+
+
+# uncomment the following test in step 5
+#def test_subtract():
+#    assert subtract(2, 3) == -1
